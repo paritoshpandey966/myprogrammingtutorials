7b96db39325c775fdcbe04681bd11e278a155a45
Paritosh Pandey <32197415+paritoshpandey966@users.noreply.github.com>
2019-11-07 21:35:11 +0530

    Update jquerymain.js

--- jquerymain.js
+++ jquerymain.js
@@ -14,11 +14,3 @@ window.addComment=function(a){function b(){c(),g()}function c(a){if(t&&(m=j(r.ca
 (function(b,l,e,g,h,f){1!==parseInt(e.msDoNotTrack||b.doNotTrack||e.doNotTrack,10)&&b.addEventListener("load",function(){var r=(new Date).getTime();b.galite=b.galite||{};var m=new XMLHttpRequest,n="https://www.google-analytics.com/collect?cid="+(l.uid=l.uid||Math.random()+"."+Math.random())+"&v=1&tid="+galite.UA+"&dl="+f(h.location.href)+"&ul=en-us&de=UTF-8",a=function(b){var d="",c;for(c in b){if(void 0===b[c])return!1;d+=f(b[c])}return d},p={dt:[h.title],sd:[g.colorDepth,"-bit"],sr:[g.availHeight,
 "x",g.availWidth],vp:[innerWidth,"x",innerHeight],dr:[h.referrer]},k;for(k in p){var q=k+"="+a(p[k]);q&&(n+="&"+q)}a=function(b,d){var c="",a;for(a in d)c="&"+a+"="+f(d[a]);return function(){var a=n+c+(galite.anonymizeIp?"&aip=1":"")+"&t="+f(b)+"&z="+(new Date).getTime();if(e.sendBeacon)e.sendBeacon(a);else try{m.open("GET",a,!1),m.send()}catch(t){(new Image).src=a}}};setTimeout(a("pageview",null),100);b.addEventListener("unload",a("timing",{utc:"JS Dependencies",utv:"unload",utt:(new Date).getTime()-
 r}))})})(window,localStorage,navigator,screen,document,encodeURIComponent);
-// kk Star Ratings
-"use strict";jQuery(document).ready(function($){function apply($el,options){var options=options||{isBusy:!1};function ajax(data,successCallback,errorCallback)
-{if(options.isBusy||$el.hasClass('kksr-disabled')){return}
-options.isBusy=!0;$.ajax({type:'POST',url:kk_star_ratings.endpoint,data:Object.assign({nonce:kk_star_ratings.nonce,action:kk_star_ratings.action},data),error:errorCallback,success:successCallback,complete:function(){options.isBusy=!1}})}
-function onClick(e){var $star=$(this);ajax({id:$el.data('id'),slug:$el.data('slug'),score:$star.data('star'),best:$('[data-star]',$el).length},function(response,status,xhr){var $newEl=$(response);$newEl.addClass($el.attr('class'));$el.replaceWith($newEl);destroy();apply($newEl,options)},function(xhr,status,err){if(xhr.responseJSON&&xhr.responseJSON.error){console.error(xhr.responseJSON.error)}})}
-function destroy(){$('[data-star]',$el).each(function(){$(this).off('click',onClick)});$el.remove()}
-$('[data-star]',$el).each(function(){$(this).on('click',onClick)})}
-$('.kk-star-ratings').each(function(){apply($(this))})})
